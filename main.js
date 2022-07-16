a0fed6c2b7e75e10251246efc95031a12e113266
EOB042502 <108642350+EOB042502@users.noreply.github.com>
2022-07-15 23:36:46 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+btn.setAttribute('class', xxx);
+btn.textContent = xxx;
+overlay.style.backgroundColor = xxx;
+
+(int i = 0; i < 10; i++), (int i = 1; i < 12; i++)
+const newImage = document.createElement('img');
+newImage.setAttribute('src', xxx);
+thumbBar.appendChild(newImage);
+
+backgroundColor"red"
